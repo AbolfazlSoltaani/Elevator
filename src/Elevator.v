3a63aafb401287c3586aa98dd6bfe35f1cc181f0
Abolfazl Soltani <abolfazl.soltani@sotoon.ir>
2022-07-22 15:27:08 +0430

    Elevator started...

--- src/Elevator.v
+++ src/Elevator.v
@@ -6,4 +6,27 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Elevator();
+module Elevator(input [4:0] request_floors, );
+
+integer current_floor = 0
+reg [4:0] ins;
+reg [4:0] outs;
+
+always @(request_floors) begin
+    ins = ins | request_floors
+end
+
+always @(current_floor) {
+    if (outs[current_floor]) {
+        outs[current_floor] = 0
+        capacity = capacity - 1
+        current_floor <- next_floor()
+    }
+    if (ins[current_floor])
+        ins[current_floor] = 0
+}
+
+endmodule
+
+// request_floors = 01000
+// ins = 01000
